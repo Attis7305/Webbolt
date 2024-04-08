6fb8e191e1893ef2151e242176362dc1d0516167
Joe <nalt@freemail.hu>
2024-04-08 14:04:04 +0200

    Masodik feltolt

--- shopapi/urls.py
+++ shopapi/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import index
+from .views import index , signup
 
 
 urlpatterns = [
-    path('', index)
+    path('', index , name='homepage'),
+    path('signup' , signup)
 ]
