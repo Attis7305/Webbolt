8d5cd30ce02448548ab14bad0af1d77b7fced7bd
Joe <nalt@freemail.hu>
2024-04-22 11:17:31 +0200

    Nyolcadik feltolt

--- shopapi/urls.py
+++ shopapi/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views.home import Index
+from .views.home import Index , shopapi
 from .views.signup import Signup
 from .views.login import Login , logout
 from .views.cart import Cart
@@ -11,10 +11,11 @@ from .middlewares.auth import auth_middleware
 
 urlpatterns = [
     path('', Index.as_view(), name='homepage'),
+    path('shopapi' , shopapi, name='shopapi'),
     path('signup' , Signup.as_view(), name='signup'),
     path('login' , Login.as_view(), name='login'),
     path('logout' , logout , name='logout'),
-path('cart' , Cart.as_view() , name='cart'),
+path('cart' , auth_middleware(Cart.as_view()) , name='cart'),
 path('check-out' , CheckOut.as_view() , name='checkout'),
 path('orders' , auth_middleware(OrderView.as_view()) , name='orders'),
 ]
